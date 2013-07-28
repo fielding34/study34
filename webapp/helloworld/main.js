bb58c08e852d94dae7b3ee11fe378854f09d6793
fielding34 <fielding.wang@gmail.com>
2013-07-28 22:56:41 +0800

    webapp hello world

--- webapp/helloworld/main.js
+++ webapp/helloworld/main.js
@@ -1,11 +1,3 @@
-// chrome.app.runtime.onLaunched.addListener(function() {
-// 	chrome.appWindow.create('window.html', {
-// 		'bounds': {
-// 			'width': 400,
-// 			'height': 500
-// 		}
-// 	});
-// })
 chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('window.html', {
 		'bounds': {
